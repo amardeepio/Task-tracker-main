d824a2a47c2e3c40b7533730d82bc285326c68e3
amardeepio <amars123456@gmail.com>
2022-02-20 09:19:53 +0530

    changes made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2022</p>
+      <p>Copyright &copy; 2023</p>
       <Link to='/about'>about</Link>
     </footer>
   )
