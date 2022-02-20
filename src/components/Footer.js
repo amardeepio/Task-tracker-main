8186200fbd24f7ac6ada52aa21d46eae4670672a
amardeepio <amars123456@gmail.com>
2022-02-20 08:59:58 +0530

    changes made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <p>Copyright &copy; 2021</p>
-      <Link to='/about'>About</Link>
+      <Link to='/about'>about</Link>
     </footer>
   )
 }
